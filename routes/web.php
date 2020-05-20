8469611c6629bd45d609fb3282cab9cdf16f4087
Ramon Lima e Meira <ramonlimaemeira@gmail.com>
2020-05-20 19:50:49 -0300

    09 - Introdução a Rotas no Laravel 6.x

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,16 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::post('/register', function() {
+    return '';
+});
+
+Route::get('/contato', function() {
+    return view('site.contact');
+});
+
+Route::get('/empresa', function() {
+    return 'Empresa';
+});
 
 Route::get('/', function () {
     return view('welcome');
