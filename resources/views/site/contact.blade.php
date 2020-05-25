14b5c49d5ee82f41a91001656eff7ce01348442c
Ramon Lima e Meira <ramonlimaemeira@gmail.com>
2020-05-25 14:46:12 -0300

    12 - Rotas no Laravel - redirect e view

--- resources/views/site/contact.blade.php
+++ resources/views/site/contact.blade.php
@@ -1,1 +1,2 @@
 <h1>Contatos</h1>
+<h2>{{$subtitle}}</h2>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,20 @@
 <?php
 
+Route::view('/view','site.contact',['subtitle'=>'Antigos']);
+
+// Route::get('/view', function () {
+//     return view('welcome');
+// });
+
+Route::redirect('/redirect1','/redirect2');
+// Route::get('/redirect1', function() {
+//     return redirect('/redirect2');
+// });
+
+Route::get('/redirect2', function() {
+    return 'redirect2';
+});
+
 Route::get('produtos/{idPordutos?}/details', function($idPordutos = '') {
     return "Produto(s): {$idPordutos}";
 });
