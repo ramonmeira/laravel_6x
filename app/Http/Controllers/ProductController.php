261ae470b15a3b0f4aa2679c860b1af84a288874
Ramon Lima e Meira <ramonlimaemeira@gmail.com>
2020-05-26 07:01:09 -0300

    18 - Controllers de CRUD no Laravel 6.x

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,4 +15,24 @@ class ProductController extends Controller
     public function show($id){
     	return "Exibindo o produto de id: {$id}";
     }
+
+    public function create(){
+    	return "Criando um produto";
+    }
+
+    public function edit($id){
+    	return "Form para editar o  produto de id: {$id}";
+    }
+
+    public function store(){
+    	return "Cadastrando um novo produto";
+    }
+
+    public function update($id){
+    	return "Editando o produto de id: {$id}";
+    }
+
+    public function destroy($id){
+    	return "Deletando o produto de id: {$id}";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+Route::delete('/products/{id}', 'ProductController@destroy')->name('products.destroy');
+Route::put('/products/{id}', 'ProductController@update')->name('products.update');
+Route::get('/products/{id}/edit', 'ProductController@edit')->name('products.edit');
+Route::get('/products/create', 'ProductController@create')->name('products.create');
 Route::get('/products/{id}', 'ProductController@show')->name('products.show');
 Route::get('/products', 'ProductController@index')->name('products.index');
+Route::post('/products', 'ProductController@store')->name('products.store');
 
 /*Route::middleware([])->group(function(){
 	Route::prefix('admin')->group(function(){
